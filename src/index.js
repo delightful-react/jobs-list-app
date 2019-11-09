555ff7f4303b93104602e66798a44aa6be447452
Bhargav Ponnapalli <bhargavponnapalli.5@gmail.com>
2019-11-09 15:21:43 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 const mountNode = document.getElementById("root");
 ReactDOM.render(
   React.createElement("p", {
-    children: "Hello World"
+    children: "Hello Create React App!"
   }),
   mountNode
 );
